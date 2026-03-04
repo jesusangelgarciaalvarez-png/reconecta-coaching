278a4bfb0a9db6263d7723a2a26e5a12fa76d7d9
Your Name <you@example.com>
2026-03-03 21:17:25 -0600

    Add automatic phone formatting to booking form

--- src/booking.js
+++ src/booking.js
@@ -199,7 +199,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Auto-focus phone or name?
+    // Phone Formatting (Mexico Style: 55 1234 5678)
+    if (phoneInput) {
+        phoneInput.addEventListener('input', (e) => {
+            let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,4})(\d{0,4})/);
+            e.target.value = !x[2] ? x[1] : x[1] + ' ' + x[2] + (x[3] ? ' ' + x[3] : '');
+        });
+    }
+
     // Start calendar
     initCalendar();
 });
